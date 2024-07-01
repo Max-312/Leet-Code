01dbea129045f26c683a6bc39ea0563a96435979
Drahar Moonstone <drahar.moonstone@mail.ru>
2024-07-01 10:42:46 +0300

    Time: 56 ms (76.95%), Space: 17.7 MB (28.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        array = {}
-        for i, num in enumerate(nums):
-            t = target - num
-            if t in array:
-                return [array[t], i]
-            array[num] = i
-        return []
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       array = {}
+       for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in array: 
+               return [i, array[remaining]]  
+           else:
+               array[value] = i 
+    
