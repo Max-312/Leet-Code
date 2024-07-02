46f8a7e9a5aca7fb54e9332a403cc5a19f3f0267
Drahar Moonstone <drahar.moonstone@mail.ru>
2024-07-02 21:05:00 +0300

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
    def twoSum(self, nums: List[int], target: int) -> List[int]:
        array = {}
-       for i, value in enumerate(nums): #1
-           remaining = target - nums[i] #2
+       for i, value in enumerate(nums): 
+           remaining = target - nums[i] 
            
            if remaining in array: 
                return [i, array[remaining]]  
